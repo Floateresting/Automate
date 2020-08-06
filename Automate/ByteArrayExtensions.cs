b7165613047956c1c7e8b9dcaaebd5ad8656220b
Floaterest <floaterest@outlook.jp>
2020-08-06 17:57:08 -0500

    Fix region matching

--- Automate/ByteArrayExtensions.cs
+++ Automate/ByteArrayExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace Automate {
+﻿using System.Drawing;
+
+namespace Automate {
     public static class ByteArrayExtensions {
         /// <summary>
         /// Indicates whether the difference between two colors are tolerable
@@ -7,7 +9,7 @@
         /// <param name="pixel2"></param>
         /// <param name="t">tolerance squared</param>
         /// <returns></returns>
-        private static bool MatchesWith(this byte[] pixel1, byte[] pixel2, int t) {
+        internal static bool MatchesWith(this byte[] pixel1, byte[] pixel2, int t) {
             int actual = 0;
             for(int i = 0; i < 3; i++) {
                 actual += (pixel1[i] - pixel2[i]) * (pixel1[i] - pixel2[i]);
@@ -24,10 +26,10 @@
         /// <param name="region2">the smaller region</param>
         /// <param name="t">tolerance squared</param>
         /// <returns></returns>
-        public static bool MatchesWidth(this byte[,][] region1, int x1, int y1, byte[,][] region2, int t) {
+        internal static bool MatchesWidth(this byte[,][] region1, int x1, int y1, byte[,][] region2, int t) {
             for(int x2 = 0; x2 < region2.GetLength(0); x2++) {
                 for(int y2 = 0; y2 < region2.GetLength(1); y2++) {
-                    if(region1[x1 + x2, y1 + y2].MatchesWith(region2[x2, y2], t)) {
+                    if(!region1[x1 + x2, y1 + y2].MatchesWith(region2[x2, y2], t)) {
                         return false;
                     }
                 }
