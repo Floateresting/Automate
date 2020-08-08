282bfa215c911d2e5d11465df43e1a02f8d1b3fe
Floaterest <floaterest@outlook.jp>
2020-08-08 17:28:18 -0500

    Remove unused methods

--- Automate/Window.cs
+++ Automate/Window.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
+﻿using System.Drawing;
 using System.Drawing.Imaging;
-using System.IO;
 using System.Runtime.InteropServices;
 
 namespace Automate {
@@ -122,73 +119,6 @@ namespace Automate {
             graphics.CopyFromScreen(position.X, position.Y, 0, 0, screenshotSize, CopyPixelOperation.SourceCopy);
 
             return screenshot;
-        }
-
-        /// <summary>
-        /// Convert a bmp image to rgb array
-        /// </summary>
-        /// <param name="bitmap">Bitmap image to convert</param>
-        /// <returns>byte[y][x][r, g, b]</returns>
-        private static byte[][][] GetBitmapArray(Bitmap bitmap) {
-            // byte[y][x][r, g, b]
-            byte[][][] bitmapArray = new byte[bitmap.Height][][];
-
-            unsafe {
-                // Me from 2019-08-29: I don't understand anything :(
-                // Me from 2019-10-24: Me neither :(
-                // Me from 2019-12-13: ME neither :(
-                BitmapData bitmapData = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height), ImageLockMode.ReadWrite, bitmap.PixelFormat);
-                int bytesPerPixel = Bitmap.GetPixelFormatSize(bitmap.PixelFormat) / 8;
-                int heightInPixels = bitmapData.Height;
-                int widthInBytes = bitmapData.Width * bytesPerPixel;
-                byte* ptrFirstPixel = (byte*)bitmapData.Scan0;
-
-                // Search for each row (why I can't use Parallel.For)
-                for(int y = 0; y < heightInPixels; y++) {
-                    bitmapArray[y] = new byte[bitmap.Width][];
-                    byte* currentLine = ptrFirstPixel + (y * bitmapData.Stride);
-                    for(int x = 0; x < widthInBytes; x += bytesPerPixel) {
-                        bitmapArray[y][x / bytesPerPixel] = new byte[] {
-                                    currentLine[x + 2],    //r
-                                    currentLine[x + 1],    //g
-                                    currentLine[x]         //b
-                                };
-                    }
-                }
-                bitmap.UnlockBits(bitmapData);
-            }
-            return bitmapArray;
-        }
-
-        /// <summary>
-        /// Check if all pixels match (only for 1 line)
-        /// </summary>
-        /// <param name="screenLine">rgb array of a pixel line from screen</param>
-        /// <param name="screenStart">position of where to start</param>
-        /// <param name="templateLine">rgb array of a pixel line from template</param>
-        /// <param name="templateStart">position of where to start</param>
-        /// <param name="toleranceSquared"></param>
-        /// <param name="actualTolerance">Actual tolerance found</param>
-        /// <returns>True if the line matches</returns>
-        private static bool SearchFor1Line(byte[][] screenLine, int screenStart, byte[][] templateLine, int templateStart, int toleranceSquared, out int actualTolerance) {
-            int maxDelta = 0;
-            for(int x = 0; x < templateLine.Length; x++) {
-                // Get rgb of the corresp pixel
-                byte[] screenRGB = screenLine[x + screenStart];
-                byte[] templateRGB = templateLine[x + templateStart];
-                int delta = 0;
-                // Add 3 (rgb) squared deltas together
-                for(int i = 0; i < 3; i++) {
-                    delta += (screenRGB[i] - templateRGB[i]) * (screenRGB[i] - templateRGB[i]);
-                }
-                if(delta > toleranceSquared) {
-                    actualTolerance = 0;
-                    return false;
-                }
-                maxDelta = Math.Max(maxDelta, delta);
-            }
-            actualTolerance = maxDelta;
-            return true;
         }
         #endregion
     }
