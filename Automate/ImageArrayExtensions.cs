2d343aca8453b608b3d9b6839de6fb30d7acda22
Floaterest <floaterest@outlook.jp>
2020-09-04 17:27:20 -0500

    Add LocateColorAll

--- Automate/ImageArrayExtensions.cs
+++ Automate/ImageArrayExtensions.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
-using System.Security.Cryptography;
 
 namespace Automate {
     public static class ImageArrayExtensions {
@@ -62,11 +61,48 @@ namespace Automate {
                     // Add rect and return point if matches
                     if(heystack.MatchesWith(x1, y1, needle, tolerance)) {
                         // Add needle with minimum distance
-                        covered.Add(new Rectangle(x1 - distance, y1 - distance, needle.Width + distance, needle.Height + distance));
+                        covered.Add(new Rectangle(
+                            x1 - distance,
+                            y1 - distance,
+                            needle.Width + distance,
+                            needle.Height + distance
+                        ));
 
                         yield return new Point(x1 + needle.Width / 2, y1 + needle.Height / 2);
                         // continue outside the needle
                         x1 += needle.Width;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Seach for a color and return all the results
+        /// </summary>
+        /// <param name="heystack"></param>
+        /// <param name="color"></param>
+        /// <param name="width">Width of the solid color region</param>
+        /// <param name="height">Width of the solid color region</param>
+        /// <param name="tolerance">Minimum distance between 2 colors</param>
+        /// <param name="distance">Minimun distance between 2 found areas</param>
+        /// <returns></returns>
+        public static IEnumerable<Point> LocateColorAll(this ImageArray heystack, byte[] color, int width, int height, int tolerance = 0, int distance = 0) {
+            tolerance *= tolerance;
+            List<Rectangle> coverd = new List<Rectangle>();
+            for(int y1 = 0; y1 < heystack.Height - height; y1++) {
+                for(int x1 = 0; x1 < heystack.Width - width; x1++) {
+                    if(coverd.Select(rect => rect.Contains(x1, y1)).Any()) continue;
+
+                    if(heystack.MatchesWith(x1, y1, color, width, height, tolerance)) {
+                        coverd.Add(new Rectangle(
+                            x1 - distance,
+                            y1 - distance,
+                            width + distance,
+                            height + distance
+                        ));
+
+                        yield return new Point(x1 + width / 2, y1 + height / 2);
+                        x1 += width;
                     }
                 }
             }
