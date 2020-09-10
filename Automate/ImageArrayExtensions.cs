178020443c624647e0648bf899fa05bb80c8eae1
Floaterest <floaterest@outlook.jp>
2020-09-09 20:27:14 -0500

    Change search from Color to Template

--- Automate/ImageArrayExtensions.cs
+++ Automate/ImageArrayExtensions.cs
@@ -1,23 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 
 namespace Automate {
     public static class ImageArrayExtensions {
-        /// <summary>
-        /// Convert a hex color to <see cref="byte[]"/>
-        /// e.g. 0x39c5bb to { 0x39, 0xc5, 0xbb }
-        /// </summary>
-        /// <param name="hex"></param>
-        /// <returns></returns>
-        public static byte[] ToRGB(this int hex) {
-            byte[] rgba = new byte[3];
-            for(int i = rgba.Length; i-- > 0; hex >>= 8) {
-                rgba[i] = (byte)(hex & 0xff);
-            }
-            return rgba;
-        }
-
         /// <summary>
         /// Search for a <see cref="ImageArray"/> and return the first match
         /// </summary>
@@ -69,59 +56,16 @@ namespace Automate {
             }
         }
 
-        /// <summary>
-        /// Search for a solid color and return the first match
-        /// </summary>
-        /// <param name="heystack"></param>
-        /// <param name="hex">0xrrggbb</param>
-        /// <param name="width">Width of the solid color region</param>
-        /// <param name="height">Width of the solid color region</param>
-        /// <param name="tolerance">Minimum distance between 2 colors</param>
-        /// <returns></returns>
-        public static Point LocateColor(this ImageArray heystack, int hex, int width, int height, int tolerance = 0) {
-            tolerance *= tolerance;
-            byte[] rgb = hex.ToRGB();
-            for(int y1 = 0; y1 <= heystack.Height - height; y1++) {
-                for(int x1 = 0; x1 < heystack.Width - width; x1++) {
-                    if(heystack.MatchesWith(x1, y1, rgb, width, height, tolerance)) {
-                        return new Point(x1 + width / 2, y1 + height / 2);
+        public static Point LocateTemplate(this ImageArray h, Template t) {
+            for(int y1 = t.Y; y1 <= h.Height - t.Height; y1++) {
+                for(int x1 = t.X; x1 <= h.Width - t.Width; x1++) {
+                    if(h.MatchesWith(x1, y1, t.Color, t.Size, t.Size, t.Tolerance2)) {
+                        int offset = t.Size / 2;
+                        return new Point(x1 + offset, y1 + offset);
                     }
                 }
             }
             return Point.Empty;
-        }
-       
-        /// <summary>
-        /// Seach for a color and return all the results
-        /// </summary>
-        /// <param name="heystack"></param>
-        /// <param name="hex">0xrrggbb</param>
-        /// <param name="width">Width of the solid color region</param>
-        /// <param name="height">Width of the solid color region</param>
-        /// <param name="tolerance">Minimum distance between 2 colors</param>
-        /// <param name="distance">Minimun distance between 2 found areas</param>
-        /// <returns></returns>
-        public static IEnumerable<Point> LocateColorAll(this ImageArray heystack, int hex, int width, int height, int tolerance = 0, int distance = 0) {
-            tolerance *= tolerance;
-            byte[] rgb = hex.ToRGB();
-            List<Rectangle> covered = new List<Rectangle>();
-            for(int y1 = 0; y1 < heystack.Height - height; y1++) {
-                for(int x1 = 0; x1 < heystack.Width - width; x1++) {
-                    if(covered.Select(rect => rect.Contains(x1, y1)).Any()) continue;
-
-                    if(heystack.MatchesWith(x1, y1, rgb, width, height, tolerance)) {
-                        covered.Add(new Rectangle(
-                            x1 - distance,
-                            y1 - distance,
-                            width + distance,
-                            height + distance
-                        ));
-
-                        yield return new Point(x1 + width / 2, y1 + height / 2);
-                        x1 += width;
-                    }
-                }
-            }
         }
     }
 }
---
+++ Automate/Template.cs
@@ -0,0 +1,42 @@
+﻿namespace Automate {
+    public struct Template {
+        public byte[] Color { get; }
+        public int Size { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public int Tolerance2 { get; }
+
+        /// <summary>
+        /// Initialize a template
+        /// </summary>
+        /// <param name="hex">Target color in hex, e.g. 0x39c5bb</param>
+        /// <param name="size">Minimum size of the color (will be searching for a square)</param>
+        /// <param name="region">Expected region (for faster template matching)</param>
+        /// <param name="tolerance">Maximum distance with the color</param>
+        public Template(int hex, int size, (int X, int Y, int Width, int Height) region, int tolerance) {
+            this.Color = Template.ToRGB(hex);
+            this.Size = size;
+            this.X = region.X;
+            this.Y = region.Y;
+            this.Width = region.Width;
+            this.Height = region.Height;
+            this.Tolerance2 = tolerance * tolerance;
+        }
+
+        /// <summary>
+        /// Convert a hex color to <see cref="byte[]"/>
+        /// e.g. 0x39c5bb to { 0x39, 0xc5, 0xbb }
+        /// </summary>
+        /// <param name="hex"></param>
+        /// <returns></returns>
+        public static byte[] ToRGB(int hex) {
+            byte[] rgb = new byte[3];
+            for(int i = rgb.Length; i-- > 0; hex >>= 8) {
+                rgb[i] = (byte)(hex & 0xff);
+            }
+            return rgb;
+        }
+    }
+}
